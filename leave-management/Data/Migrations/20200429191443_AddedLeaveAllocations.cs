3c99d58c2f6cb13185a155851dfc467c0e7b989d
Garon <garonross@hotmail.co.uk>
2020-05-02 08:12:16 +0100

    Add project files.

---
+++ leave-management/Data/Migrations/20200429191443_AddedLeaveAllocations.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leave_management.Data.Migrations
+{
+    public partial class AddedLeaveAllocations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveHistory_AspNetUsers_ApprovedById",
+                table: "LeaveHistory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveHistory_LeaveTypes_LeaveTypeId",
+                table: "LeaveHistory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveHistory_AspNetUsers_RequestingEmployeeId",
+                table: "LeaveHistory");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LeaveHistory",
+                table: "LeaveHistory");
+
+            migrationBuilder.RenameTable(
+                name: "LeaveHistory",
+                newName: "LeaveHistories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LeaveHistory_RequestingEmployeeId",
+                table: "LeaveHistories",
+                newName: "IX_LeaveHistories_RequestingEmployeeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LeaveHistory_LeaveTypeId",
+                table: "LeaveHistories",
+                newName: "IX_LeaveHistories_LeaveTypeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LeaveHistory_ApprovedById",
+                table: "LeaveHistories",
+                newName: "IX_LeaveHistories_ApprovedById");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LeaveHistories",
+                table: "LeaveHistories",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "LeaveAllocations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberOfDays = table.Column<int>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    EmployeeId = table.Column<string>(nullable: true),
+                    LeaveTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveAllocations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveAllocations_AspNetUsers_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LeaveAllocations_LeaveTypes_LeaveTypeId",
+                        column: x => x.LeaveTypeId,
+                        principalTable: "LeaveTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveAllocations_EmployeeId",
+                table: "LeaveAllocations",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveAllocations_LeaveTypeId",
+                table: "LeaveAllocations",
+                column: "LeaveTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveHistories_AspNetUsers_ApprovedById",
+                table: "LeaveHistories",
+                column: "ApprovedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveHistories_LeaveTypes_LeaveTypeId",
+                table: "LeaveHistories",
+                column: "LeaveTypeId",
+                principalTable: "LeaveTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveHistories_AspNetUsers_RequestingEmployeeId",
+                table: "LeaveHistories",
+                column: "RequestingEmployeeId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveHistories_AspNetUsers_ApprovedById",
+                table: "LeaveHistories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveHistories_LeaveTypes_LeaveTypeId",
+                table: "LeaveHistories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveHistories_AspNetUsers_RequestingEmployeeId",
+                table: "LeaveHistories");
+
+            migrationBuilder.DropTable(
+                name: "LeaveAllocations");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LeaveHistories",
+                table: "LeaveHistories");
+
+            migrationBuilder.RenameTable(
+                name: "LeaveHistories",
+                newName: "LeaveHistory");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LeaveHistories_RequestingEmployeeId",
+                table: "LeaveHistory",
+                newName: "IX_LeaveHistory_RequestingEmployeeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LeaveHistories_LeaveTypeId",
+                table: "LeaveHistory",
+                newName: "IX_LeaveHistory_LeaveTypeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LeaveHistories_ApprovedById",
+                table: "LeaveHistory",
+                newName: "IX_LeaveHistory_ApprovedById");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LeaveHistory",
+                table: "LeaveHistory",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveHistory_AspNetUsers_ApprovedById",
+                table: "LeaveHistory",
+                column: "ApprovedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveHistory_LeaveTypes_LeaveTypeId",
+                table: "LeaveHistory",
+                column: "LeaveTypeId",
+                principalTable: "LeaveTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveHistory_AspNetUsers_RequestingEmployeeId",
+                table: "LeaveHistory",
+                column: "RequestingEmployeeId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
